c5f0955cfdf368ef1bdcb1913f388fa2a39f6f99
ToeWai27 <100681046+ToeWai27@users.noreply.github.com>
2023-08-15 09:11:03 +0630

    ui finished with dammy data

--- components/authentication/authLayout.js
+++ components/authentication/authLayout.js
@@ -1,10 +1,19 @@
 import { View, Text } from "react-native";
 import React from "react";
+import AuthIcon from "../../assets/icons/authIcon";
 
 export default function AuthLayout() {
   return (
-    <View>
-      <Text>AuthLayout</Text>
+    <View className="flex-1 bg-white">
+      <AuthIcon />
+      <View
+        style={{
+          borderTopLeftRadius: 40,
+          borderTopRightRadius: 40,
+          backgroundColor: "#d4a574",
+        }}
+        className="px-5 flex flex-1 justify-between  pt-8 -mt-14"
+      ></View>
     </View>
   );
 }
--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,25 +1,122 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "../screens/LoginScreen";
 import HomeScreen from "../screens/HomeScreen";
+import { Dimensions, LogBox, Platform, Text, View } from "react-native";
+
+import { themeColors } from "../theme";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  HomeIcon as HomeOutline,
+  HeartIcon as HeartOutline,
+  ShoppingBagIcon as BagOutline,
+} from "react-native-heroicons/outline";
+import {
+  HomeIcon as HomeSolid,
+  HeartIcon as HeartSolid,
+  ShoppingBagIcon as BagSolid,
+} from "react-native-heroicons/solid";
+import LoginScreen from "../screens/LoginScreen";
+import ProductScreen from "../screens/ProductScreen";
+import { heightPercentageToDP } from "react-native-responsive-screen";
+
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const ios = Platform.OS == "ios";
+LogBox.ignoreLogs([
+  "Non-serializable values were found in the navigation state",
+]);
+
 export default function AppNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        screenOptions={{
+          contentStyle: { backgroundColor: "white" },
+        }}
+        initialRouteName="Home"
+      >
         <Stack.Screen
           name="Login"
-          component={LoginScreen}
           options={{ headerShown: false }}
+          component={LoginScreen}
         />
         <Stack.Screen
           name="Home"
-          component={HomeScreen}
           options={{ headerShown: false }}
+          component={HomeTabs}
+        />
+        <Stack.Screen
+          name="Product"
+          options={{ headerShown: false }}
+          component={ProductScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ focused }) => menuIcons(route, focused),
+        tabBarStyle: {
+          marginBottom: 20,
+          height: heightPercentageToDP(8),
+          alignItems: "center",
+          borderRadius: 100,
+          marginHorizontal: 20,
+          backgroundColor: themeColors.bgLight,
+        },
+        tabBarItemStyle: {
+          alignItems: "center",
+          display: "flex",
+          height: "auto",
+          marginTop: heightPercentageToDP(2),
+        },
+      })}
+    >
+      <Tab.Screen name="home" component={HomeScreen} />
+      <Tab.Screen name="favourite" component={HomeScreen} />
+      <Tab.Screen name="cart" component={HomeScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const menuIcons = (route, focused) => {
+  let icon;
+
+  if (route.name === "home") {
+    icon = focused ? (
+      <HomeSolid size="30" color={themeColors.bgLight} />
+    ) : (
+      <HomeOutline size="30" strokeWidth={2} color="white" />
+    );
+  } else if (route.name === "favourite") {
+    icon = focused ? (
+      <HeartSolid size="30" color={themeColors.bgLight} />
+    ) : (
+      <HeartOutline size="30" strokeWidth={2} color="white" />
+    );
+  } else if (route.name === "cart") {
+    icon = focused ? (
+      <BagSolid size="30" color={themeColors.bgLight} />
+    ) : (
+      <BagOutline size="30" strokeWidth={2} color="white" />
+    );
+  }
+
+  let buttonClass = focused ? "bg-white" : "";
+  // console.log("foucsed", focused);
+  return (
+    <View
+      className={"flex items-center rounded-full p-3 shadow " + buttonClass}
+    >
+      {icon}
+    </View>
+  );
+};
